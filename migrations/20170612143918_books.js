6cb13eed0b8aa324ba04f698fb522d53f07f48a2
SSabu <sandeepsabu09@gmail.com>
2017-06-12 15:23:57 -0700

    Part 1: Migration and Seeds files added

---
+++ migrations/20170612143918_books.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  // UP adds / creates tables / fields to the DB
+  return knex.schema.createTable('books', (table) => {
+    table.increments('id').primary();
+    table.string('title').notNullable().defaultTo('');
+    table.string('author').notNullable().defaultTo('');
+    table.string('genre').notNullable().defaultTo('');
+    table.text('description').notNullable().defaultTo('');
+    table.text('cover_url').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  // DROP deletes /drops tables/fields from DB
+  return knex.schema.dropTable('books');
+
+};
